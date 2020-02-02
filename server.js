f57742e69352b0022ba1aa1f279cbe1d74d439b6
massaoud <massaoud10@gmail.com>
2020-02-02 19:14:40 +0400

    update of server.js

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 const path = require('path')
 
 
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+
 const PORT = process.env.port || 3000;
 app.listen(PORT,() => {
     console.log(work);
